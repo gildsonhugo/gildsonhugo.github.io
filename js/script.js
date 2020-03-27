324258d057f2e2afe25bbd20d964de9462942ef5
gildsonhugo <gildson.hugo@gmail.com>
2020-03-27 18:11:08 -0300

    efeito hamburguer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 document.getElementById('mobile').addEventListener('click', e => {
     document.querySelector('.navbar ul').classList.toggle('open');
+    e.currentTarget.classList.toggle('open');
 });
